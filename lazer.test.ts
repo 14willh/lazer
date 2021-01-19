32eda232dd659955cba983d96a7dea864a7fa626
Will Henderson <14willh@gmail.com>
2021-01-19 23:08:08 +0000

    Add tests for else

--- lazer.test.ts
+++ lazer.test.ts
@@ -90,7 +90,79 @@ Deno.test('Printer #elseif not should print following statements when condition 
     assertEquals(context.stdout.includes("this is false"), false);
 });
 
-Deno.test('Printer #else', () => 
+Deno.test('Printer #else should not print if previous if block entered', () => 
 {
+    const printer = lazer();
+    const context = { stdout: '' };
+    decorate_echo(printer, context);
 
+    printer
+        .if(true)
+        .print_ln("this is true")
+        .elseif(false)
+        .print_ln("this is false")
+        .end();
+
+    assertEquals(context.stdout.includes("this is true"), true);
+    assertEquals(context.stdout.includes("this is false"), false);
+});
+
+Deno.test('Printer #else should not print if previous if block entered', () => 
+{
+    const printer = lazer();
+    const context = { stdout: '' };
+    decorate_echo(printer, context);
+
+    printer
+        .if(true)
+        .print_ln("this is true")
+        .elseif(false)
+        .print_ln("this is false elseif")
+        .else()
+        .print_ln("this is false else")
+        .end();
+
+    assertEquals(context.stdout.includes("this is true"), true);
+    assertEquals(context.stdout.includes("this is false elseif"), false);
+    assertEquals(context.stdout.includes("this is false else"), false);
+});
+
+Deno.test('Printer #else should not print if previous elseif block entered', () => 
+{
+    const printer = lazer();
+    const context = { stdout: '' };
+    decorate_echo(printer, context);
+
+    printer
+        .if(false)
+        .print_ln("this is false if")
+        .elseif(true)
+        .print_ln("this is true")
+        .else()
+        .print_ln("this is false else")
+        .end();
+
+    assertEquals(context.stdout.includes("this is false if"), false);
+    assertEquals(context.stdout.includes("this is true"), true);
+    assertEquals(context.stdout.includes("this is false else"), false);
+});
+
+Deno.test('Printer #else should print if previous if/elseif block not entered', () => 
+{
+    const printer = lazer();
+    const context = { stdout: '' };
+    decorate_echo(printer, context);
+
+    printer
+        .if(false)
+        .print_ln("this is false if")
+        .elseif(false)
+        .print_ln("this is false elseif")
+        .else()
+        .print_ln("this is true")
+        .end();
+
+    assertEquals(context.stdout.includes("this is false if"), false);
+    assertEquals(context.stdout.includes("this is false elseif"), false);
+    assertEquals(context.stdout.includes("this is true"), true);
 });
