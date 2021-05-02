d456e313f1f281985f24d6f0ce96216ea9258d92
Will Henderson <14willh@gmail.com>
2021-05-02 21:14:39 +0100

    Generate node.js package with index.js naming

--- scripts/build_lazer_js.ts
+++ scripts/build_lazer_js.ts
@@ -1,7 +1,9 @@
+const outDir = 'nodejs';
+
 const duplicate = async () => 
 {
     const p = Deno.run({
-        cmd: ["cp", "lazer.ts", "lazer-js.ts"],
+        cmd: ["cp", "lazer.ts", `${outDir}/index.ts`],
     });
     const { code } = await p.status(); // (*1); wait here for child to finish
     p.close();
@@ -10,7 +12,7 @@ const duplicate = async () =>
 const compile = async () => 
 {
     const p = Deno.run({
-        cmd: ["npx", "tsc", "lazer-js.ts"],
+        cmd: ["npx", "tsc", `${outDir}/index.ts`],
     });
     const { code } = await p.status(); // (*1); wait here for child to finish
     p.close();
@@ -19,7 +21,7 @@ const compile = async () =>
 const minify = async () => 
 {
     const p = Deno.run({
-        cmd: ["node", "scripts/js-minify.js", "lazer-js.js", "lazer-js.min.js"],
+        cmd: ["node", "scripts/js-minify.js", `${outDir}/index.js`, `${outDir}/index.js`],
     });
     const { code } = await p.status(); // (*1); wait here for child to finish
     p.close();
@@ -28,13 +30,14 @@ const minify = async () =>
 const cleanup = async () => 
 {
     const p = Deno.run({
-        cmd: ["rm", "lazer-js.ts"],
+        cmd: ["rm", `${outDir}/index.ts`],
     });
     const { code } = await p.status(); // (*1); wait here for child to finish
     p.close();
 }
 
 // main
+await Deno.mkdir(outDir, { recursive: true });
 await duplicate();
 await compile();
 await minify();
